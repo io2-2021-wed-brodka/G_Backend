7ef4abcbde640bcef6822c3009e311bc88806e6a
Tomek Karwowski <to.karwowski@gmail.com>
2021-04-11 19:17:04 +0200

    bump Django to 3.2, take settings from env if present

--- salty_bikes/settings.py
+++ salty_bikes/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "6f$wiat71em%*zf&yj93d10qt-wf7lr$fr3_j!&h)=k+71f#np"
+SECRET_KEY = os.environ.get("SECRET_KEY", "6f$wiat71em%*zf&yj93d10qt-wf7lr$fr3_j!&h)=k+71f#np")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", True)
 
 ALLOWED_HOSTS = []
 
